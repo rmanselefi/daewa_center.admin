1dbf9cda30e70289e54005f78d84906c2f82e2c0
rmanselefi <kebederaz@gmail.com>
2025-11-20 10:56:45 +0300

    feat: add shadcn components

---
+++ hooks/useUser.ts
@@ -0,0 +1,41 @@
+"use client";
+
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { AuthService } from "@/services/auth.service";
+
+export function useLogin() {
+  const qc = useQueryClient();
+  return useMutation({
+    mutationFn: async ({
+      email,
+      password,
+    }: {
+      email: string;
+      password: string;
+    }) => {
+      const response = await AuthService.login(email, password);
+      return response;
+    },
+    onSuccess: () => {
+      qc.invalidateQueries({ queryKey: ["user"] });
+    },
+    onError: (error) => {
+      console.error(error);
+    },
+  });
+}
+
+export function useLogout() {
+  const qc = useQueryClient();
+  return useMutation({
+    mutationFn: async () => {
+      await AuthService.LogOut();
+    },
+    onSuccess: () => {
+      qc.invalidateQueries({ queryKey: ["user"] });
+    },
+    onError: (error) => {
+      console.error(error);
+    },
+  });
+}
